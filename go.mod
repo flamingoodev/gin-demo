ab69ff29031b3a018ba2a933fc18834a90b7b1be
FLAMINGO <flamingodev@outlook.com>
2020-06-02 00:40:36 +0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gin-demo
+
+go 1.23
+
+require github.com/gin-gonic/gin v1.6.3
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.LoadHTMLGlob("templates/*")
+	engine.Static("/static", "./static")
+	engine.GET("/hello-world", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello World!")
+	})
+	engine.GET("/json", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"username": "admin",
+			"age":      18,
+		})
+	})
+	engine.GET("/xml", func(context *gin.Context) {
+		context.XML(http.StatusOK, gin.H{
+			"username": "admin",
+			"age":      18,
+		})
+	})
+	engine.GET("/index", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.html", gin.H{
+			"name":  "admin",
+			"age":   24,
+			"names": []string{"张三", "李四", "王五"},
+		})
+	})
+	engine.GET("/get", func(context *gin.Context) {
+		name := context.Query("name")
+		age := context.Query("age")
+		fmt.Println(name)
+		fmt.Println(age)
+		context.String(http.StatusOK, "success: name="+name+",age="+age)
+	})
+	engine.POST("/post", func(context *gin.Context) {
+		name := context.PostForm("name")
+		age := context.PostForm("age")
+		fmt.Println(name)
+		fmt.Println(age)
+		context.String(http.StatusOK, "success: name="+name+",age="+age)
+	})
+	engine.GET("/user/:id", func(context *gin.Context) {
+		id := context.Param("id")
+		context.String(http.StatusOK, "success: id="+id)
+	})
+	err := engine.Run()
+	if err != nil {
+		fmt.Println("http server error")
+		return
+	}
+}
